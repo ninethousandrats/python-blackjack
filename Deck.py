fd61d3c67986c1c1d4c532f58214904301e95ab7
Jonah Crary <13083199+ninethousandrats@users.noreply.github.com>
2022-04-29 01:00:42 -0700

    Update Deck.py
    You, too, can save up to 17%* by switching to len().
    
    *+/- 0.01%

--- Deck.py
+++ Deck.py
@@ -13,41 +13,32 @@ import random
 class Deck:    
     def __init__(self):
         self.cards = []
-        self.deckSize = 0
-
         self.discardedCards = []
-        self.discardSize = 0
 
         for s in ["♠","♥","♦","♣"]:
             for r in ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]:
-                self.deckSize += 1
-                self.cards.append(Card(s,r,self.deckSize))
-
+                self.cards.append(Card(s,r,len(self.cards)))
+                
     #debug method
     def list_cards(self):
-        for x in range(self.deckSize):
+        for x in range(len(self.cards)):
             #print(self.cards[x].value)
             print(self.cards[x].suit, self.cards[x].rank)
             
     #debug method
     def list_discardedCards(self):
         print("Discard Pile:")
-        for x in range(self.discardSize):
+        for x in range(len(self.discardedCards)):
             #print(self.discardedCards[x].value)
             print(self.discardedCards[x].suit, self.discardedCards[x].rank)
             
     def deal_card(self):
-        # this could be cleaner somehow        
-        tempCard = []
-        tempCard.append(self.cards[0])
+        tempCard = self.cards[0]
         self.cards.pop(0)
-        self.deckSize -= 1
-        return tempCard[0]
+        return tempCard
     
     def add_to_discard(self, tempCard):
         self.discardedCards.insert(0, tempCard)
-        self.discardSize += 1
-        print("Discarded:", self.discardedCards[0].suit, self.discardedCards[0].rank)
     
     def shuffle_deck(self):
         random.shuffle(self.cards)
