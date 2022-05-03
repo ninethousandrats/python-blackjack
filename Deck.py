e1e08b3e1ddc2d8b09aa91ee2524ba6ade62c305
Jonah Crary <13083199+ninethousandrats@users.noreply.github.com>
2022-05-02 23:33:48 -0700

    Update Deck.py

--- Deck.py
+++ Deck.py
@@ -1,10 +1,11 @@
 """
 __init__(self)
-list_cards(self) #remove later
-list_discardedCards(self) #remove later
-deal_card(self)
+deal_card(self, quantity)
 add_to_discard(self, tempCard)
-shuffle_deck(self)
+shuffle(self)
+
+list_cards(self)
+list_discardedCards(self)
 """
 
 from Card import Card
@@ -16,9 +17,24 @@ class Deck:
         self.discardedCards = []
 
         for s in ["♠","♥","♦","♣"]:
+            assignedCardValue = 1
             for r in ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]:
-                self.cards.append(Card(s,r,len(self.cards)))
-                
+                self.cards.append(Card(s,r,assignedCardValue))
+                if assignedCardValue < 10: assignedCardValue += 1
+            
+    def deal_card(self):
+        tempCard = self.cards[0]
+        self.cards.pop(0)
+        return tempCard
+    
+    def add_to_discard(self, tempCard):
+        self.discardedCards.insert(0, tempCard)
+    
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+
+
     #debug method
     def list_cards(self):
         for x in range(len(self.cards)):
@@ -31,14 +47,3 @@ class Deck:
         for x in range(len(self.discardedCards)):
             #print(self.discardedCards[x].value)
             print(self.discardedCards[x].suit, self.discardedCards[x].rank)
-            
-    def deal_card(self):
-        tempCard = self.cards[0]
-        self.cards.pop(0)
-        return tempCard
-    
-    def add_to_discard(self, tempCard):
-        self.discardedCards.insert(0, tempCard)
-    
-    def shuffle_deck(self):
-        random.shuffle(self.cards)
