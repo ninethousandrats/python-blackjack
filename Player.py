39edb99835a2b54b31bf0f29d1c5bc1b97289447
Jonah Crary <13083199+ninethousandrats@users.noreply.github.com>
2022-05-10 00:07:40 -0700

    Update Player.py

--- Player.py
+++ Player.py
@@ -4,6 +4,10 @@ draw_card(self, newCard)
 discard_card(self, index)
 get_hand_value(self)
 show_hand(self)
+
+doubleDown(self)
+split(self)
+surrender(self)
 """
 
 from Card import Card
@@ -12,6 +16,7 @@ class Player:
     def __init__(self):
         self.hand = []
         self.handValue = 0
+        self.chips = 50000
         
     def draw_card(self, newCard):
         self.hand.insert(0, newCard)
@@ -24,7 +29,27 @@ class Player:
             self.handValue += self.hand[x].value
         return self.handValue
         
-    def show_hand(self):
-        for x in range(len(self.hand)):
-            print(self.hand[x].suit, self.hand[x].rank)
+    def show_hand(self, allCards = True):
+        if allCards:
+            for x in range(len(self.hand)):
+                print(self.hand[x].suit, self.hand[x].rank)
+        else:
+            print(self.hand[0].suit, self.hand[0].rank)
+            print("▯","▯")
+            
+    def placeBet(self, bet):
+        if bet <= self.chips:
             pass
+    
+    def doubleDown(self, newCard):
+        # double player bet
+        self.hand.insert(0, newCard)
+        print("double or nothin")
+        
+    def split(self):
+        print("cut my hand into pieces")
+        
+    def surrender(self):
+        if len(self.hand) == 2:
+            print("half bet")
+        # half bet
