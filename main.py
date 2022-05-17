a407cf1e64a9e80ec97ed9108755f603855a86aa
Jonah Crary <13083199+ninethousandrats@users.noreply.github.com>
2022-05-16 22:28:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys 
+
 from Deck import Deck
 from Player import Player
 
@@ -10,6 +12,7 @@ deck.shuffle()
 gameIsRunning = True
 
 while gameIsRunning:
+    print("Player Chips: $", player.chips)
     player.placeBet(input("How much would you like to bet? $"))
     
     # setup the round
@@ -17,22 +20,31 @@ while gameIsRunning:
         player.draw_card(deck.deal_card())
         dealer.draw_card(deck.deal_card())
     
-    dealer.show_hand(False)
-    player.show_hand()
-    
     # get player decision
     
     decision = "0"
     
-    while decision != "2" and decision != "5":
+    while decision != "2" and decision != "5" and player.chips > 0:
+        print("Dealer:")
+        dealer.show_hand(False)
+        print("Player:")
+        player.show_hand()
+    
         decision = input("1) Hit     2) Stand     3) Double Down\n4) Split   5) Surrender 6) Exit Game\n\nSelect Option: ")
         
         if decision == "1": player.draw_card(deck.deal_card())
         elif decision == "2": print("STANDING")
         elif decision == "3": player.doubleDown(deck.deal_card())
-        elif decision == "4": player.split()
+        elif decision == "4": player.split() #headache, this one
         elif decision == "5": player.surrender()
-        elif decision == "6": print("Exit Function Goes Here")
+        elif decision == "6": sys.exit()
         else: print("Invalid input, please try again.")
+        
+        if decision == "1" or "2" or "3":
+            pass
+        
+        if player.get_hand_value() > 21:
+                pass
+                #lose hand
 
     break
